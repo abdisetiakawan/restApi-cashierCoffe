c0bcb3e1dab4c2c4ef9e662c901be463713d516b
abdisetiakawan <bedikadiryt@gmail.com>
2024-09-19 08:30:15 +0700

    "Refactor database connection configuration in database.js"

--- config/database.js
+++ config/database.js
@@ -2,8 +2,12 @@ const { Sequelize } = require("sequelize");
 require("dotenv").config();
 
 const sequelize = new Sequelize(
-  `mysql://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`,
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASS,
   {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
     dialect: "mysql",
   }
 );
